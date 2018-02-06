e30e4ec6e718bb9041c7b758927211eee0cc842f
equalizer <faruk.yerli@synchron.ua>
2018-02-06 19:56:43 +0300

    R&D-Basic Table Implementation

--- src/components/pages/Tests/tables1.jsx
+++ src/components/pages/Tests/tables1.jsx
@@ -1,17 +1,81 @@
-import React, { Component } from "react";
-import {Link} from 'react-router-dom';
+import React from "react";
+import { makeData } from "./utils";
+import TableHeader from "./table.header";
+import ReactTable from "react-table";
 
-export default class Tables1 extends Component {
+export default class Tables1 extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: makeData(100)
+    };
+  }
+
   render = () => {
+    const { data } = this.state;
     return (
-      <div id="content">
-        <h3> Tables1 PAGE </h3>
-        Tables Page 1
-        <br/>
-        <br/>
-        <Link to="/">
-        Home
-        </Link>
+      <div id="content" style={{ opacity: 1 }}>
+        <article className="col-xs-12 col-sm-12 col-md-12 col-lg-12 sortable-grid ui-sortable">
+          <div
+            className="jarviswidget jarviswidget-color-blueDark jarviswidget-sortable"
+            id="wid-id-0"
+            data-widget-editbutton="false"
+          
+          >
+            <TableHeader />
+            <div id="content">
+              <div className="jarviswidget-editbox" />
+
+              <div className="widget-body">
+                <div className="table-responsive">
+                  <ReactTable
+                    data={data}
+                    columns={[
+                      {
+                        Header: "Name",
+                        columns: [
+                          {
+                            Header: "First Name",
+                            accessor: "firstName"
+                          },
+                          {
+                            Header: "Last Name",
+                            id: "lastName",
+                            accessor: d => d.lastName
+                          }
+                        ]
+                      },
+                      {
+                        Header: "Info",
+                        columns: [
+                          {
+                            Header: "Age",
+                            accessor: "age"
+                          },
+                          {
+                            Header: "Status",
+                            accessor: "status"
+                          }
+                        ]
+                      },
+                      {
+                        Header: "Stats",
+                        columns: [
+                          {
+                            Header: "Visits",
+                            accessor: "visits"
+                          }
+                        ]
+                      }
+                    ]}
+                    defaultPageSize={10}
+                    className="-striped -highlight"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
       </div>
     );
   };
